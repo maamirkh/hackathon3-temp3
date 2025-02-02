7ce4b9c68728d24d81930fac1cc9b52c3a2cdfbd
Your Name <you@example.com>
2025-02-02 16:28:35 +0500

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,5 @@
 // Components
 
-import Header from "./components/Header";
-// import Hero from './components/hero';
-// import Shoes from "./components/Shoes";
-// import Featured from "./components/featured";
-// import Gear from "./components/GearUp";
-// import Essential from "./components/Essential";
-import Footer from "./components/Footer";
-
-import Cards from "./components/Header";
-
-import Images from "./pics/gearup/Images.png";
 import Hero from "./components/Hero";
 import Shoes from "./components/Shoes";
 import Featured from "./components/Featured";
